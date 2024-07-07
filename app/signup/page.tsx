d0798500803f4ec01b084bb2f4d69c588bb1a7d8
Shubham <shubhamsinghmau2@gmail.com>
2024-07-07 17:37:16 +0530

    feat: signin functionality

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,67 +1,74 @@
-'use client'
+"use client";
 
-import React from 'react';
-import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React from "react";
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
+import axios from "axios";
 
 interface FieldType {
-  username: string,
-  password: string,
-  remember?: string
+  username: string;
+  password: string;
+  email: string;
 }
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log("success", values);
-}
+const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+  try {
+    const response = await axios.post(``, {
+      username: values.username,
+      email: values.email,
+      password: values.password
+    });
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('Axios Error:', error);
+  }
+};
 
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
 };
 
 const SignUp: React.FC = (): JSX.Element => {
   return (
     <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
     >
-      <Input />
-    </Form.Item>
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item<FieldType>
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "Please input your email!" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{ offset: 8, span: 16 }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-  )
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default SignUp;
-
