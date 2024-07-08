fcae7225e640ac20edbf502762a655c658385d33
Shubham <shubhamsinghmau2@gmail.com>
2024-07-08 20:40:32 +0530

    fix: dashboard redirect to signup

--- store/user.tsx
+++ store/user.tsx
@@ -1,16 +1,20 @@
-import create from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import create from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const useStore = create<any>(persist(
-  (set) => ({
-    username: '',
-    login: (name: string) => set({ username: name }),
-    logout: () => set({ username: '' }),
-  }),
-  {
-    name: 'user-storage',
-    storage: createJSONStorage(() => localStorage), 
-  }
-));
+const initialUsername = localStorage.getItem("username") || "";
+
+const useStore = create<any>(
+  persist(
+    (set) => ({
+      username: initialUsername,
+      login: (name: string) => set({ username: name }),
+      logout: () => set({ username: "" }),
+    }),
+    {
+      name: "user-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useStore;
