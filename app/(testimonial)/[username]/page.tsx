1419c38af998770a8e7bc978c98fb09f377d9315
Shubham <shubhamsinghmau2@gmail.com>
2024-07-08 15:57:14 +0530

    feat: dynamic feedback upload

--- app/(testimonial)/[username]/page.tsx
+++ app/(testimonial)/[username]/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { StarFilled } from "@ant-design/icons";
 import styled from "styled-components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { FormProps } from "antd";
-import { renderToString } from "react-dom/server";
-import { Button, Checkbox, Flex, Form, Input } from "antd";
 import useNotify from "@/hooks/useNotification";
+import { Button, Flex, Form, Input } from "antd";
+import axios from "axios";
+import { useParams } from "next/navigation";
 
 interface FieldType {
   username: string;
-  password: string;
-  remember?: string;
+  feedback: string;
+  email: string;
 }
 
 const rating = (
@@ -23,48 +24,87 @@ const rating = (
   </>
 );
 
-const StyledFlex = styled(Flex)`
-  width: 100%;
-  @media (max-width: 1000px) {
-    flex-direction: column;
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-  h1 {
-    padding: 0.5rem;
-  }
-  p {
-    padding: 0.5rem;
-  }
-  section {
-    p {
-      font-weight: bold;
-    }
-  }
-  ul {
-    padding: 1rem;
-  }
-  @media (max-width: 1000px) {
-    width: 90%;
-  }
-`;
-
-const FormContainer = styled(Form)`
-width: 60%;
-    @media (max-width: 1000px) {
-      width: 90%;
-  }
-`
-
 const Testimonial: React.FC = (): JSX.Element => {
   const { TextArea } = Input;
+  const params = useParams();
+
+  const { notify, contextHolder } = useNotify();
+
+  const developerUsername = params?.username;
+
+
+  const StyledFlex = styled(Flex)`
+    width: 100%;
+    @media (max-width: 1000px) {
+      flex-direction: column;
+    }
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    h1 {
+      padding: 0.5rem;
+    }
+    p {
+      padding: 0.5rem;
+    }
+    section {
+      p {
+        font-weight: bold;
+      }
+    }
+    ul {
+      padding: 1rem;
+    }
+    @media (max-width: 1000px) {
+      width: 90%;
+    }
+  `;
+
+  const FormContainer = styled(Form)`
+    width: 60%;
+    @media (max-width: 1000px) {
+      width: 90%;
+    }
+  `;
+
+  const handleNotify = (
+    placement: "topLeft" | "topRight" | "bottomLeft" | "bottomRight"
+  ) => {
+    notify({
+      message: "Registered!",
+      description: "Your review has been successfully registered!",
+      placement,
+    });
+  };
+
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+    if (!developerUsername) {
+      console.error("Developer username is not defined in the URL.");
+      return;
+    }
+
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/reviews/${developerUsername}/new`,
+        {
+          review_description: values.feedback,
+          reviewer_username: values.username,
+          reviewer_email: values.email,
+        }
+      );
+      handleNotify("topRight");
+    } catch (error) {
+      console.error("Axios Error:", error);
+    }
+  };
+
   return (
     <StyledFlex justify="center" align="center">
+      {contextHolder}
       <Container>
         <section>
           <h1>Testify Our Work</h1>
@@ -79,39 +119,48 @@ const Testimonial: React.FC = (): JSX.Element => {
           </section>
         </section>
       </Container>
-      <FormContainer
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        layout="vertical"
-        initialValues={{ remember: true }}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label={rating}
-          rules={[{ required: true, message: "Please give your feedback" }]}
+      {developerUsername ? (
+        <FormContainer
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          layout="vertical"
+          style={{ width: "60%" }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
         >
-          <TextArea rows={4} placeholder="Feedback" />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Your Name"
-          rules={[{ required: true, message: "Please input your name!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Your Email"
-          rules={[{ required: true, message: "Please input your email!" }]}
-        >
-          <Input />
-        </Form.Item>
+          <Form.Item<FieldType>
+            name="feedback"
+            label={rating}
+            rules={[{ required: true, message: "Please give your feedback" }]}
+          >
+            <TextArea rows={4} placeholder="Feedback" />
+          </Form.Item>
+          <Form.Item<FieldType>
+            name="username"
+            label="Your Username"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            name="email"
+            label="Your Email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </FormContainer>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </FormContainer>
+      ) : (
+        <p>Loading...</p>
+      )}
     </StyledFlex>
   );
 };
